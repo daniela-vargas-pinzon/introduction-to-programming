d0d381208d63045df08666484ed7dbb0e34ab59c
daniela-vargas-pinzon <78397182+daniela-vargas-pinzon@users.noreply.github.com>
2021-03-26 10:59:27 -0500

    Add files via upload

---
+++ EXERCICES JAVA/P1T41_average_height_parcial/P1T41_average_height/P1T37_METODS/src/usta/fis/Main.java
@@ -0,0 +1,25 @@
+package usta.fis;
+/* AUTHOR   : DANIELA VARGAS
+   DATE     : 23 March of 2021
+   DESCRIPTION This software use methods in your code
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        p_show_info_program();
+        p_show_info_program();
+        p_show_info_program();
+        p_show_info_program();
+    }
+
+
+    public static void p_show_info_program() {
+        //DATE       : 23 March of 2021
+        //DESCRIPTION: this method show the info software
+        System.out.println("-------------------------");
+        System.out.println("-SOFTMETHOD VERSION 1.0--");
+        System.out.println("MAKER: Felipe Lopez");
+        System.out.println("DESCRIPTION: Used methods");
+        System.out.println("-------------------------");
+    }
+}
