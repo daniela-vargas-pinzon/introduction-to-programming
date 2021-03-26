2cf611b8f8a61a010de49ba8ce90cb3725206fdb
daniela-vargas-pinzon <78397182+daniela-vargas-pinzon@users.noreply.github.com>
2021-03-26 10:15:25 -0500

    Add files via upload

---
+++ EXERCICES JAVA/P1T41_average_height_parcial/P1T41_average_height/P1T34_HELLO_WORLD/src/usta/fis/Main.java
@@ -0,0 +1,16 @@
+package usta.fis;
+//AUTOR: Daniela Vargas
+//DATE: 16/03/2021
+//DESCRIPTION: this is my first program in java
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("__________________________");
+        System.out.println("softhelloworld version 1.0")
+        System.out.println("maker:daniela vargas      ")
+        System.out.println("description: this program show hello world")
+        System.out.println("__________________________")
+        System.out.println("hello world")
+    }
+}
